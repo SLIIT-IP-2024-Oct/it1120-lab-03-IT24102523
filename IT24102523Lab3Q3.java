289607f974da2cba57f829ece04f05e269725f99
IT24102523 <it24102523@my.sliit.lk>
2024-11-22 12:21:52 +0530

    Update IT24102523Lab3Q3.java

--- IT24102523Lab3Q3.java
+++ IT24102523Lab3Q3.java
@@ -1,20 +1,56 @@
 import java.util.Scanner;
 
-public class IT24102523Lab3Q3 {
+public class IT24102523LabQ3 {
 
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the rupee amount: ");
+        System.out.println("Enter the Amount : ");
         int amount = input.nextInt();
 
-        int[] notes = {5000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
+        int Notes5000 = amount / 5000;
+        amount -= Notes5000 * 5000;
 
-        for (int note : notes) {
-            int count = amount / note;
-            amount %= note;
-            System.out.println(note + " Notes - " + count);
-        }
+        int Notes1000 = amount / 1000;
+        amount -= Notes1000 * 1000;
 
+        int Notes500 = amount / 500;
+        amount -= Notes500 * 500;
 
+        int Notes200 = amount / 200;
+        amount -= Notes200 * 200;
+
+        int Notes100 = amount / 100;
+        amount -= Notes100 * 100;
+
+        int Notes50 = amount / 50;
+        amount -= Notes50 * 50;
+
+        int Notes20 = amount / 20;
+        amount -= Notes20 * 20;
+
+        int Notes10 = amount / 10;
+        amount -= Notes10 * 10;
+
+        int Notes5 = amount / 5;
+        amount -= Notes5 * 5;
+
+        int Notes2 = amount / 2;
+        amount -= Notes2 * 2;
+
+        int Notes1 = amount;
+
+        System.out.println("5000 Notes : " + Notes5000);
+        System.out.println("1000 Notes : " + Notes1000);
+        System.out.println("500 Notes : " + Notes500);
+        System.out.println("200 Notes : " + Notes200);
+        System.out.println("100 Notes : " + Notes100);
+        System.out.println("50 Notes : " + Notes50);
+        System.out.out.println("20 Notes : " + Notes20);
+        System.out.println("10 Notes : " + Notes10);
+        System.out.println("5 Notes : " + Notes5);
+        System.out.println("2 Notes : " + Notes2);
+        System.out.println("1 Notes : " + Notes1);
     }
 }
+
